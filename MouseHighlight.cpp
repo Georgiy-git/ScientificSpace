a9a3cc26cda1f770820b01d22075ed42bac53d65
Georgiy <graubin1@mail.ru>
2025-08-07 23:43:56 +0300

    07.08.2025-23:43

--- MouseHighlight.cpp
+++ MouseHighlight.cpp
@@ -1,12 +1,9 @@
 #include "MouseHighlight.hpp"
 
 MouseHighlight::MouseHighlight(SDL_Renderer* render)
-	: render{render}
+	: Object(render, -100, -100, 0, 0)
 {
-	area_color[0] = 123;
-	area_color[1] = 200;
-	area_color[2] = 246;
-	area_color[3] = 255;
+	set_texture("textures\\videlenie.svg", 400, 400);
 }
 
 void MouseHighlight::event_process(SDL_Event* event)
@@ -14,25 +11,25 @@ void MouseHighlight::event_process(SDL_Event* event)
 	if (event->type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
 		down_x = event->button.x;
 		down_y = event->button.y;
-		area.x = down_x;
-		area.y = down_y;
+		size_texture_frame.x = down_x;
+		size_texture_frame.y = down_y;
 		down = true;
 		need_render = false;
 	}
 	else if (event->type == SDL_EVENT_MOUSE_MOTION && down) {
 		if (event->button.y > down_y) {
-			area.h = event->button.y - area.y;
+			size_texture_frame.h = event->button.y - size_texture_frame.y;
 		}
 		else {
-			area.y = event->button.y;
-			area.h = down_y - event->button.y;
+			size_texture_frame.y = event->button.y;
+			size_texture_frame.h = down_y - event->button.y;
 		}
 		if (event->button.x > down_x) {
-			area.w = event->button.x - area.x;
+			size_texture_frame.w = event->button.x - size_texture_frame.x;
 		}
 		else {
-			area.x = event->button.x;
-			area.w = down_x - event->button.x;
+			size_texture_frame.x = event->button.x;
+			size_texture_frame.w = down_x - event->button.x;
 		}
 		if (!need_render) need_render = true;
 	}
@@ -42,8 +39,7 @@ void MouseHighlight::event_process(SDL_Event* event)
 }
 
 void MouseHighlight::iterate() {
-	if (need_render && area.w > 1 && area.h > 1) {
-		SDL_SetRenderDrawColor(render, area_color[0], area_color[1], area_color[2], area_color[3]);
-		SDL_RenderFillRect(render, &area);
+	if (need_render && size_texture_frame.w > 1 && size_texture_frame.h > 1) {
+		Object::iterate();
 	}
 }
--- MouseHighlight.hpp
+++ MouseHighlight.hpp
@@ -3,20 +3,19 @@
 #include <SDL3/SDL.h>
 #include <array>
 
-class MouseHighlight
+#include "Object.hpp"
+
+class MouseHighlight : Object
 {
 public:
 	MouseHighlight(SDL_Renderer* render);
 
 	void event_process(SDL_Event* event);
 
-	void iterate();
-
-	std::array<uint8_t, 4> area_color;
+	void iterate() override;
 	
 private:
 	SDL_Renderer* render = nullptr;
-	SDL_FRect area;
 	float down_x = -1;
 	float down_y = -1;
 	bool down = false;
